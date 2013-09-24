a058da10a9260724888b6622237877ad59f969cf
Nonoo <nonoo@nonoo.hu>
2013-09-24 11:39:01 +0200

    added nai module which will handle usb receive events and main alarm interface logic

---
+++ hostapp/nai.c
@@ -0,0 +1,5 @@
+#include "nai.h"
+
+void nai_usb_packet_received_cb(nai_usbpacket_t *usbpacket) {
+	// TODO
+}
---
+++ hostapp/nai.h
@@ -0,0 +1,9 @@
+#ifndef NAI_H_
+#define NAI_H_
+
+#include "types.h"
+#include "../common/usbprotocol.h"
+
+void nai_usb_packet_received_cb(nai_usbpacket_t *usbpacket);
+
+#endif
