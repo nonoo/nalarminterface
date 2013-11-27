645f2590642e2402322b9eb82ca8eec5611d940f
root <root@iszapszivo.localdomain>
2013-11-27 17:22:14 +0100

    replaced action on p4 int with p3 int in naialarm.sh

--- hostapp/naialarm.sh
+++ hostapp/naialarm.sh
@@ -22,10 +22,10 @@ if [ ! -z $p1int ] && [ $p1int -eq 1 ]; then
 	echo -n "interrupt on p1 - "
 	if [ $p1state -eq 1 ]; then
 		echo "state on, setting foscam md alarm 1."
-		../foscamctrl/foscam-setmdalarm 1
+#		../foscamctrl/foscam-setmdalarm 1
 	else
 		echo "state off, setting foscam md alarm 0."
-		../foscamctrl/foscam-setmdalarm 0
+#		../foscamctrl/foscam-setmdalarm 0
 	fi
 fi
 
@@ -50,11 +50,7 @@ fi
 if [ ! -z $p3int ] && [ $p3int -eq 1 ]; then
 	echo -n "interrupt on p3 - "
 	if [ $p3state -eq 1 ]; then
-		echo "state on, sending sound alarm email."
-		subject="[nai] A hang erzekelo bekapcsolt"
-		msg="A szenzor hangot erzekelt: `date`"
-		$scriptdir/../mail/mail.sh nonoo@nonoo.hu "$subject" "$msg"
-#		$scriptdir/../mail/mail.sh dianno@dianno.hu "$subject" "$msg"
+		echo "state on, doing nothing."
 	else
 		echo "state off, doing nothing."
 	fi
@@ -63,7 +59,11 @@ fi
 if [ ! -z $p4int ] && [ $p4int -eq 1 ]; then
 	echo -n "interrupt on p4 - "
 	if [ $p4state -eq 1 ]; then
-		echo "state on, doing nothing."
+		echo "state on, sending sound alarm email."
+		subject="[nai] A hang erzekelo bekapcsolt"
+		msg="A szenzor hangot erzekelt: `date`"
+		$scriptdir/../mail/mail.sh nonoo@nonoo.hu "$subject" "$msg"
+#		$scriptdir/../mail/mail.sh dianno@dianno.hu "$subject" "$msg"
 	else
 		echo "state off, doing nothing."
 	fi
